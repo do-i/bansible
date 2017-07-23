36dedffef5a5b0b498bf2b55395da99af257e54e
Ninja <ninja@renton.net>
2017-07-23 18:08:41 -0500

    [FIX] Removed report and top level directory from the output of tree

--- roles/webapp/files/tree_helper.sh
+++ roles/webapp/files/tree_helper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /mnt && tree -J | jq . > /var/www/html/data/media_files_list_v3.json
+cd /mnt && tree --noreport -J | jq .[0].contents > /var/www/html/data/media_files_list_v3.json
