2c93887ee4e615150868b5cdfe6689102e58d53d
Ninja <ninja@renton.net>
2019-01-13 12:42:57 -0600

    [rpi3A+] Supports Raspberry Pi 3 Model A+ without eth0 interface

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # Bansible ~ Bubble3 Configured By Ansible
-### Setup Raspberry Pi with Raspbian Jessie Lite (headless mode)
+### Setup Raspberry Pi with Raspbian Strech Lite (headless mode)
+
+#### Step 0: Download Raspbian Strech Lite OS
+- Download OS from https://www.raspberrypi.org/downloads/raspbian/ to home directory
+
 #### Step 1: Run raspbianizer.sh
 - Specify the correct device path to the fresh SD card (e.g. /dev/sdd, /dev/sde, or /dev/sdx)
 - Specify the BRANCH variable with correct github branch or release version tag (master, v1.0.0, etc)
+- Specify WIFI_SSID and WIFI_PASS which has Internet connection.
 - Example:
 ```sh
-export BRANCH=release-branch-2017.08.13 && curl -skL "https://raw.githubusercontent.com/do-i/bansible/${BRANCH}/scripts/raspbianizer.sh" | sudo bash -s /dev/sdx
+export BRANCH=release-branch-2019.01.13 && curl -skL "https://raw.githubusercontent.com/do-i/bansible/${BRANCH}/scripts/raspbianizer.sh" | sudo WIFI_SSID="changeme" WIFI_PASS="changeit" bash -s /dev/sdx ~/2018-11-13-raspbian-stretch-lite.zip
 ```
 
-#### Step 2: Boot-up Raspberry Pi3 using SD card & SSH
-- Take out the SD card from your computer and put it in Raspberry Pi3
-- Check that an ethernet cable(Cat 5) connects between Raspberry Pi3 and the router(has Internet access)
-- Ensure that the USB flash drive holding your data is connected to the Pi3 before powering up
+#### Step 2: Boot-up Raspberry Pi 3 Model A+ using SD card & SSH
+- Take out the SD card from your computer and put it in Raspberry Pi 3 Model A+
+- Ensure that the USB flash drive holding your data is connected to the Pi 3 before powering up
 - SSH into Bubble
 
 #### Step 3: Install and Configure (Run on Bubble)
 Recommend you to use same BRANCH as in Step 1.
 ```sh
-export BRANCH=release-branch-2017.08.13 && curl -skL "https://raw.githubusercontent.com/do-i/bansible/${BRANCH}/install_local.sh" | sudo BRANCH="${BRANCH}" bash
+export BRANCH=release-branch-2019.01.13 && curl -skL "https://raw.githubusercontent.com/do-i/bansible/${BRANCH}/install_local.sh" | sudo BRANCH="${BRANCH}" bash
 ```
 - Note: Thanks for a [great instructional article](https://frillip.com/using-your-raspberry-pi-3-as-a-wifi-access-point-with-hostapd) for WiFi configuration.
 - Override Options
@@ -27,7 +31,7 @@ export BRANCH=release-branch-2017.08.13 && curl -skL "https://raw.githubusercont
   ```
   - WPA Passphrase (alphanumeric 8-63 characters)
   ```sh
-  export PASS=MySecretPassPhrase2016
+  export PASS=MySecretPassPhrase2020
   ```
 
 #### Step 4: Check for Errors & Reboot
--- scripts/raspbianizer.sh
+++ scripts/raspbianizer.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-# Copyright (c) 2017 Joji Doi
-# This bash script automates manual installation of raspbian OS
-# Require sdcard to be unmounted and no partition should be allocated
-OS=$(uname)
-DEVICE_NAME="${1}"
-RASPBIAN="2017-07-05-raspbian-jessie-lite"
+# Copyright (c) 2019 Joji Doi
+#
+# Create raspbian image on sdcard
 
-RASPBIAN_URL="http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2017-07-05/2017-07-05-raspbian-jessie-lite.zip"
-WORK_DIR=$(pwd)
-
-## check if the ${DEVICE_NAME} exists
-if [ "${DEVICE_NAME}" == "" ]; then
-  echo "Usage: bash ${0} <DEVICE_NAME> #e.g., /dev/sdx"
-  exit 1
-fi
-echo "DEVICE_NAME:${DEVICE_NAME}"
-echo "RASPBIAN:${RASPBIAN}"
-echo "WORK_DIR:${WORK_DIR}"
-
-### Check if the device name ends with a digit (partition number).
 function find_partition_count_linux() {
+  ### Check if the device name ends with a digit (partition number).
   if [[ ${DEVICE_NAME} =~ [0-9]$ ]]; then
     echo "Do not specify device partition. ${DEVICE_NAME}"
     exit 1
@@ -52,7 +37,6 @@ function delete_partitions_mac() {
     echo "Partition(s) found. They will be wiped out."
     diskutil eraseDisk JHFS+ Bubble ${DEVICE_NAME}
   fi
-
   diskutil unmountDisk ${DEVICE_NAME}
 }
 
@@ -60,52 +44,71 @@ function delete_partitions_linux() {
   if [ ${CNT} -gt 1 ]; then
     echo "Partition(s) found. They will be wiped out."
     for i in $(parted -s ${DEVICE_NAME} print | awk '/^ / {print $1}'); do
-      parted -s ${DEVICE_NAME} rm ${i}
+      sudo parted -s ${DEVICE_NAME} rm ${i} > /dev/null 2>&1
     done
   fi
 }
 
-function download_upzip() {
-  echo "Erase everything on ${DEVICE_NAME} and copy ${RASPBIAN}"
-  if [ ! -f ${WORK_DIR}/${RASPBIAN}.img ]; then
-    if [ ! -f ${WORK_DIR}/${RASPBIAN}.zip ]; then
-      echo "Donwloading ${RASPBIAN} (~300MB)... takes a several minutes."
-      curl -skL "${RASPBIAN_URL}" --output "${WORK_DIR}/${RASPBIAN}.zip"
-      ## verify the downloads
-      if [ ! -e "${WORK_DIR}/${RASPBIAN}.zip" ]; then
-        echo "download failed..."
-        exit 1
-      fi
-    fi
-    unzip ${WORK_DIR}/${RASPBIAN}.zip
-    if [ ! -e "${WORK_DIR}/${RASPBIAN}.img" ]; then
-        echo "unzip failed..."
-        exit 1
-    fi
-  fi
-}
-
 function copy_image_to_device() {
-  echo "Copying ${WORK_DIR}/${RASPBIAN}.img to ${DEVICE_NAME}... takes about 7 to 8 minutes. Kick back and relax."
-  sudo dd bs=4194304 if=${WORK_DIR}/${RASPBIAN}.img of=${DEVICE_NAME}
-  echo "Sync in progress... do not touch nothing."
+  echo "Copying ${RASPBIAN} to ${DEVICE_NAME}... takes about 18 minutes. Take a quick nap."
+  unzip -p ${RASPBIAN} | sudo dd of=${DEVICE_NAME} bs=4194304 conv=fsync
+  echo "Sync in progress... do not touch nothing... takes about 7 to 8 minutes. Kick back and relax."
   sync
 }
 
-function create_ssh_file() {
+function create_ssh_wpa_files() {
   local BUBBLE=/tmp/bubble
   local BOOT_MNT=${DEVICE_NAME}1
   sudo mkdir -p ${BUBBLE}
   sudo mount ${BOOT_MNT} ${BUBBLE}
-  touch ${BUBBLE}/ssh
+  sudo touch ${BUBBLE}/ssh
   if [ -f ${BUBBLE}/ssh ]; then
     echo "Enable sshd"
   else
     echo "Failed to create ssh file in boot partition."
     exit 1
   fi
+  sudo tee /tmp/bubble/wpa_supplicant.conf &>/dev/null<<EOF
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=US
+
+network={
+	ssid="${WIFI_SSID}"
+	psk="${WIFI_PASS}"
+  key_mgmt=WPA-PSK
+}
+EOF
+  sudo tee /tmp/bubble/interfaces &>/dev/null<<EOF
+auto lo
+iface lo inet loopback
+
+allow-hotplug wlan0
+iface wlan0 inet manual
+    wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
+EOF
   sudo umount ${BOOT_MNT}
 }
+
+### End of functions
+
+DEVICE_NAME="${1}"
+RASPBIAN="${2}"
+OS=$(uname)
+
+## check if the ${DEVICE_NAME} exists
+if [ "${DEVICE_NAME}" == "" ] || [ "${RASPBIAN}" == "" ]; then
+  echo "Usage: ${0} <DEVICE_NAME> <RASPBIAN_ZIP>"
+  echo "Example: ${0} /dev/sdx ~/tmps/2018-11-13-raspbian-stretch-lite.zip"
+  exit 1
+fi
+if [ ! -f "${RASPBIAN}" ]; then
+  echo "${RASPBIAN} is not a valid file"
+  exit 1
+fi
+echo "DEVICE_NAME: ${DEVICE_NAME}"
+echo "RASPBIAN:    ${RASPBIAN}"
+
 
 if [ "${OS}" == "Darwin" ]; then
   find_partition_count_mac && check_device_status && delete_partitions_mac
@@ -117,7 +120,7 @@ else
 fi
 
 ## TODO check return status code from the previous command.
-download_upzip && copy_image_to_device && create_ssh_file \
+copy_image_to_device && create_ssh_wpa_files \
   && echo "Script completed. Check for errors in case there is any." \
   && exit 0
 
